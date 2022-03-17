6cee6bee29f7d9bcff97170cf91a69b0edf368e9
tugs <tugsguru@gmail.com>
2022-03-17 22:59:48 +0800

    Event listeneruudiig uusgej ugluu

---
+++ app.js
@@ -0,0 +1,40 @@
+var uiController = (function(){
+ 
+})();
+
+var financeController=(function(){
+
+})();
+
+var appController=(function(uiController,financeController){
+
+    var ctrlAddItem = function(){
+
+        //1. Oruulah ugugliig delgetsees olj avna 
+        console.log("1.9999")
+
+        //2. Olj avsan ugugdluudee sanhuugiin controllert damjuulj tend hadgalna 
+
+        //3. olj avsan ugugdluudee web deeree tohiroh hesegt ni gargana 
+
+        //4. Tusviig tootsoolno 
+
+        //5. Etssiin uldegdel, tootsoog delgetsend gargana 
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click',function(){
+       
+       
+    });
+
+    document.addEventListener('keypress',function(event){
+
+        if(event.keyCode === 13 || event.which === 13) {
+            
+            console.log("enter pressed")
+        }
+        
+    });
+
+
+})(uiController,financeController);
